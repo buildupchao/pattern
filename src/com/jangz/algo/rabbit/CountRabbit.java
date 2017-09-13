d237ca46ffa4a208c576b29df37eb8a55612a104
Jang Zhang <jangz@synnex.com>
2017-09-13 14:19:14 +0800

    Fibonacci

---
+++ src/com/jangz/algo/rabbit/CountRabbit.java
@@ -0,0 +1,15 @@
+package com.jangz.algo.rabbit;
+
+class Rabbit {
+	public int born(int n) {
+		return (n == 0 || n == 1 || n == 2) ? 1 : born(n - 1) + born(n - 2); 
+	}
+}
+
+public class CountRabbit {
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println(new Rabbit().born(i));
+		}
+	}
+}
