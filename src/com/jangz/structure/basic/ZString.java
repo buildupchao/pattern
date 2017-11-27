2577f384af6d3d36e2eba07b1412cdb43eb15077
Jang Zhang <jangz@synnex.com>
2017-11-27 13:54:56 +0800

    realizing class ZList

---
+++ src/com/jangz/structure/basic/ZString.java
@@ -0,0 +1,41 @@
+package com.jangz.structure.basic;
+
+import java.io.Serializable;
+
+public final class ZString implements Serializable, Comparable<ZString>, CharSequence {
+
+	private static final long serialVersionUID = -104198781891118592L;
+
+	private final char[] value;
+	
+	private int hash;
+	
+	public ZString() {
+		this.value = "".toCharArray();
+	}
+	
+	@Override
+	public int length() {
+		return value.length;
+	}
+
+	@Override
+	public char charAt(int index) {
+		return 0;
+	}
+	
+/*	public byte[] getBytes() {
+		return StringCoding
+	}*/
+
+	@Override
+	public CharSequence subSequence(int start, int end) {
+		return null;
+	}
+
+	@Override
+	public int compareTo(ZString o) {
+		return 0;
+	}
+
+}
