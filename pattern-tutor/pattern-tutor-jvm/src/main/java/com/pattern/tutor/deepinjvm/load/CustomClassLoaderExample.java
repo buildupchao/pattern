aa0a3fcf90dd4c1af35e7833a5380fea69f57a16
Jang Zhang <jangz@synnex.com>
2018-04-25 16:13:52 +0800

    the time of initialization Java Object

--- pattern-tutor/pattern-tutor-jvm/src/main/java/com/pattern/tutor/deepinjvm/load/CustomClassLoaderExample.java
+++ pattern-tutor/pattern-tutor-jvm/src/main/java/com/pattern/tutor/deepinjvm/load/CustomClassLoaderExample.java
@@ -4,11 +4,11 @@ public class CustomClassLoaderExample {
 
 	public static void main(String[] args) {
 		CustomClassLoader classLoader = new CustomClassLoader();
-		classLoader.setRoot("D:\\jangz\\git\\vm");
+		classLoader.setRoot("D:/jangz/git/projects/pattern/pattern-tutor/pattern-tutor-syntax/src/main/java/");
 		
 		Class<?> testClass = null;
 		try {
-			testClass = classLoader.loadClass("com.pattern.tutor.deepinjvm.load.WaitLoadExample");
+			testClass = classLoader.loadClass("com.pattern.tutor.syntax.scary.ScaryExpressionExample");
 			Object instance = testClass.newInstance();
 			System.out.printf("ClassLoader is %s.\n", instance.getClass().getClassLoader());
 		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
