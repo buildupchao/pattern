aa0fac1cbbbc74f10d1bac85a0cd3b132410349c
zhangyachao <zhangyachao@wps.cn>
2019-08-16 11:23:33 +0800

    delete hive event table.

---
+++ pattern-tutor/pattern-tutor-syntax/src/main/java/com/pattern/tutor/syntax/database/util/DbHiveTableCleanHelper.java
@@ -0,0 +1,48 @@
+package com.pattern.tutor.syntax.database.util;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class DbHiveTableCleanHelper {
+	
+	public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
+		String workspace = System.getProperty("user.dir");
+		String base = "src/main/java/com/pattern/tutor/syntax/database";
+		String fileOutputPath = workspace + "/" + base;
+		String clearHiveTableSqlData = "clearHiveTable.data";
+		BufferedReader sqlReader = new BufferedReader(new InputStreamReader(new FileInputStream(fileOutputPath + "/" + clearHiveTableSqlData)));
+		
+		String url = "jdbc:hive2://xxx:xxx/default";
+		Class.forName("org.apache.hive.jdbc.HiveDriver");
+		Properties connectionProps = new Properties();
+        connectionProps.put("user", "");
+        connectionProps.put("password", "");
+		Connection connection = DriverManager.getConnection(url, connectionProps);
+		Statement statement = connection.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+		String sql = null;
+		while (StringUtils.isNoneBlank((sql = sqlReader.readLine()))) {
+			try {
+				System.out.println(sql);
+				statement.execute(sql);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				System.out.println("no handle!");
+			}
+		}
+		
+		if (sqlReader != null) {
+			sqlReader.close();
+		}
+		System.out.println("DONE");
+	}
+}
